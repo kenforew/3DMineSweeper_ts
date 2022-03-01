2f79d251e5ee13fa56429a275a599d9d5f92f6a0
YTNKMR <50067504+kenforew@users.noreply.github.com>
2022-03-01 14:23:15 +0900

    Update 3DMS.ts

--- 3DMS.ts
+++ 3DMS.ts
@@ -726,9 +726,9 @@ function gameDisplay(): void {
 }
 
 if(navigator.userAgent.indexOf("iPhone") > 0
-|| navigator.userAgent.indexOf("iPhone") > 0
-|| navigator.userAgent.indexOf("iPhone") > 0
-|| navigator.userAgent.indexOf("iPhone") > 0) {
+|| navigator.userAgent.indexOf("iPad") > 0
+|| navigator.userAgent.indexOf("iPod") > 0
+|| navigator.userAgent.indexOf("Android") > 0) {
     canvas.addEventListener("touchstart", mouseDown);
     canvas.addEventListener("touchmove", mouseMove);
     canvas.addEventListener("touchend", mouseUp);
